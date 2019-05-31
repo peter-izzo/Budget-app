8f855480e1680608acea3934144a95547c5cca1e
Peter Izzo <peter.izzo528@gmail.com>
2019-05-31 06:25:15 -0400

    app totals inc and exp at top and gives percentage of exp on top also

--- app.js
+++ app.js
@@ -108,7 +108,11 @@ var UIController = (function() {
       inputValue: '.add__value',
       inputBtn: '.add__btn',
       incomeContainer: '.income__list',
-      expensesContainer: '.expenses__list'
+      expensesContainer: '.expenses__list',
+      budgetLabel: '.budget__value',
+      incomeLabel: '.budget__income--value',
+      expenseLabel: '.budget__expenses--value',
+      percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -161,6 +165,21 @@ var UIController = (function() {
 
       },
 
+      displayBudget: function(obj) {
+
+        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
+        if(obj.percentage > 0) {
+          document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+        } else {
+          document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+        }
+
+      },
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -191,7 +210,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
 
     //3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
 
   };
 
@@ -222,6 +241,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   return{
     init: function() {
       console.log('Application has started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };

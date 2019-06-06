49b6db71e9957d137d639571bb5c1ba89be6dafe
Peter Izzo <peter.izzo528@gmail.com>
2019-06-06 06:36:41 -0400

    Small percentages on list items

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ var UIController = (function() {
       incomeLabel: '.budget__income--value',
       expenseLabel: '.budget__expenses--value',
       percentageLabel: '.budget__expenses--percentage',
-      container: '.container'
+      container: '.container',
+      expensesPercLabel: '.item__percentage'
     };
 
     return {
@@ -233,6 +234,26 @@ var UIController = (function() {
 
       },
 
+      displayPercentages: function(percentages) {
+
+          var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+          var nodeListForEach = function(list, callback) {
+              for (var i = 0; i < list.length; i++) {
+                callback(list[i], i);
+              }
+          };
+
+          nodeListForEach(fields, function(current, index) {
+
+              if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+              } else {
+                current.textContent = '---';
+              }
+          });
+      },
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -279,7 +300,7 @@ var controller = (function (budgetCtrl, UICtrl) {
       var percentages = budgetCtrl.getPercentages();
 
       //update the UI with the new percentages
-      console.log(percentages);
+      UICtrl.displayPercentages(percentages);
 
   };
 
